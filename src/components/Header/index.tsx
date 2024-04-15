7af2dcb9cf25d1faffe5d96622c0909158b38a74
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-15 14:02:23 +0200

    chore: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import './index.css'
+import { useNavigate } from 'react-router-dom'
+
+const Header: () => JSX.Element = () => {
+  const navigate = useNavigate()
+  const handleLogoClick: () => void = () => {
+    navigate('/')
+  }
+
+  return (
+    <div className='header'>
+      <h5
+        className="pressable"
+        onClick={handleLogoClick}
+      >
+          Podcaster
+      </h5>
+    </div>
+  )
+}
+
+export default Header
--- src/screens/RootLayout/index.tsx
+++ src/screens/RootLayout/index.tsx
@@ -1,13 +1,12 @@
-// import HomeScreen from '../HomeScreen'
-
 import { Outlet } from 'react-router-dom'
-
 import './index.css'
+
+import Header from '../../components/Header'
 
 const RootLayout: () => JSX.Element = () => {
   return (
     <div className='layout'>
-      <h2>Header</h2>
+      <Header />
       <Outlet />
     </div>
   )
