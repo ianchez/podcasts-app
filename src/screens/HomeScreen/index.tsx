44cbc1f3f028e7de37177750a57d335bffb4b1a3
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-16 00:47:38 +0200

    chore: WIP - listing podcast

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,16 +1,40 @@
+import { useState } from 'react'
+import usePodcasts from '../../hooks/usePodcasts'
+
 const HomeScreen: () => JSX.Element = () => {
+  const { isLoading, data = [] } = usePodcasts();
+  const [filter, setFilter] = useState('');
+
+  console.log({data});
+
+  const onFilterChange = (
+    {target}: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFilter(target.value);
+  }
+
+  console.log({filter})
+
   return (
     <div className="screen">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <div className='row'>
+        <p>100</p>
+        <input
+          type='text'
+          placeholder='Filter podcasts...'
+          value={filter}
+          onChange={onFilterChange}
+        />
+      </div>
+
+      {isLoading && <p>Loading...</p>}
+      {data?.feed?.entry && (
+        <ul>
+          {data.feed.entry.map((podcast: any) => (
+            <li key={podcast.id.attributes['im:id']}>{podcast['im:name'].label}</li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
