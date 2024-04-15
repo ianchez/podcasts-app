395f9fe8e0209bde281f78fe2fcb8bd1f77d4839
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-15 22:12:38 +0200

    refactor: api service to be a class module

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -2,16 +2,23 @@ const PODCASTS_URL = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genr
 const PODCAST_DETAIL_URL = (id: string, limit = 20): string =>
     `https://itunes.apple.com/lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=${limit}`
 
-const getData = async (url: string): Promise<any> => {
-  const response = await fetch(url)
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`)
+class ApiService {
+  static async getData(url: string): Promise<any> {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    return await response.json()
   }
-  return await response.json()
+
+  async getPodcasts(): Promise<any> {
+    return await ApiService.getData(PODCASTS_URL)
+  }
+
+  async getPodcastDetail(id: string, limit?: number): Promise<any> {
+    return await ApiService.getData(PODCAST_DETAIL_URL(id, limit))
+  }
 }
 
-export const getPodcasts = async (): Promise<any> =>
-  await getData(PODCASTS_URL)
-
-export const getPodcastDetail = async (id: string, limit?: number): Promise<any> =>
-  await getData(PODCAST_DETAIL_URL(id, limit))
+const apiService = new ApiService()
+export default apiService
