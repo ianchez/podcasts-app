d1704d26fcd906d53ab551621cedaf96dffe73e2
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-16 19:54:34 +0200

    chore: adding podcast detail into podcasts context provider

--- src/contexts/podcasts.tsx
+++ src/contexts/podcasts.tsx
@@ -1,15 +1,23 @@
 import React, { createContext, useEffect, useState } from 'react';
+
 import usePodcasts from '../hooks/usePodcasts';
+import usePodcastDetailById from '../hooks/usePodcastDetailById';
 import { Podcast } from '../constants/types';
 
 type podcastsState = {
   isLoading: boolean;
   podcasts: Podcast[];
+  podcastDetail: any;
+  episodes: any[];
+  setPodcastId: (id: string) => void;
 }
 
 const DEFAULT_STATE: podcastsState = {
   isLoading: true,
-  podcasts: []
+  podcasts: [],
+  podcastDetail: null,
+  episodes: [],
+  setPodcastId: () => {},
 }
 
 export const PodcastsContext = createContext(DEFAULT_STATE);
@@ -20,17 +28,43 @@ type PodcastsProviderProps = {
 
 export const PodcastsProvider = ({ children }: PodcastsProviderProps) => {
   const [ podcasts, setPodcasts ] = useState<Podcast[]>([]);
+  const [ podcastId, setPodcastId ] = useState('');
+  const [ podcastDetail, setPodcastDetail ] = useState<Podcast | null>(null);
+  const [ episodes, setEpisodes ] = useState<any[]>([]);
 
-  const { isLoading, data } = usePodcasts();
+  const { isLoading: isPodcastsLoading, data } = usePodcasts();
+  const { isLoading: isPodcastDetailLoading, data: podcastDetailData } = usePodcastDetailById(podcastId);
 
   useEffect(() => {
-    if (!isLoading) {
+    if (!isPodcastsLoading) {
       setPodcasts(data?.feed?.entry as Podcast[] || []);
     }
-  } , [isLoading]);
+  } , [isPodcastsLoading]);
+
+  useEffect(() => {
+    if (!isPodcastDetailLoading) {
+      const detailData = podcastDetailData?.results.find((data: any) => data.kind === "podcast");
+      const episodeData = podcastDetailData?.results.filter((data: any) => data.kind === "podcast-episode");
+      if (detailData) {
+        setPodcastDetail(detailData as Podcast);
+      }
+
+      if (episodeData?.length) {
+        setEpisodes(episodeData as Podcast[]);
+      }
+    }
+  } , [isPodcastDetailLoading, podcastId]);
+
+  const value = {
+    isLoading: isPodcastsLoading || isPodcastDetailLoading,
+    podcasts,
+    podcastDetail,
+    episodes,
+    setPodcastId,
+  }
 
   return (
-    <PodcastsContext.Provider value={{ isLoading, podcasts }}>
+    <PodcastsContext.Provider value={value}>
       {children}
     </PodcastsContext.Provider>
   );
