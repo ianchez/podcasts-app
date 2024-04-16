705a646e46531f29496b5e3dfc522bc2e170eab4
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-16 19:53:49 +0200

    refactor: removed unnecesary link since topbar allows user to go back to home screen

--- src/screens/NotFoundScreen/index.tsx
+++ src/screens/NotFoundScreen/index.tsx
@@ -1,11 +1,6 @@
-import { Link } from 'react-router-dom';
-
 const NotFoundScreen = () => {
   return (
     <div className='screen'>
-      <p>
-        <Link to="/">Back to Home</Link>
-      </p>
       <h3>Not found</h3>
     </div>
   );
