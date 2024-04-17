3b4dac99f3cf6ea5e4cddb818189087dda6253cd
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-17 13:32:07 +0200

    chore: added home podcasts list style

--- src/components/PodcastsList/index.tsx
+++ src/components/PodcastsList/index.tsx
@@ -10,28 +10,82 @@ const PodcastList: React.FC<PodcastListProps> = ({
   podcasts,
   filter = ''
 }) => {
-  const navigate = useNavigate();
 
   const filteredPodcasts: any[] = podcasts?.filter((podcast: any) => 
     podcast['im:name'].label.toLowerCase().includes(filter.toLowerCase())
   ) || [];
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        marginTop: '8%',
+        columnGap: '1vw',
+        rowGap: '6vw',
+        width: '100%',
+      }}
+    >
+      {filteredPodcasts.map((podcast: Podcast) =>
+        <PodcastCard key={podcast.id.attributes['im:id']} podcast={podcast} />
+      )}
+    </div>
+  );
+}
+
+const PodcastCard: React.FC<{podcast: Podcast}> = ({podcast}) => {
+  const navigate = useNavigate();
+
+  const id = podcast.id.attributes['im:id'] || '';
 
   const onEpisodeClickHandler = (id: string) => {
     navigate(`/podcast/${id}`);
   };
 
   return (
-    <ul>
-      {filteredPodcasts.map((podcast: any) => {
-        const id = podcast.id.attributes['im:id'] || '';
-
-        return (
-          <li className="pressable" key={podcast.id.attributes['im:id']} onClick={() => onEpisodeClickHandler(id)}>
-            {podcast['im:name'].label}
-          </li>
-        );
-      })}
-    </ul>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'start',
+        alignItems: 'center',
+        padding: 0,
+        height: "100%",
+        minWidth: '200px',
+        width: '23%',
+      }}
+    >
+      <div style={{ height: '100px', width: '100%' }}></div>
+      <div
+        className="pressable"
+        onClick={() => onEpisodeClickHandler(id)}
+        style={{
+          boxShadow: '0px 0px 8px 0px #ccc',
+          boxSizing: 'border-box',
+          padding: '0px 1vw 1vw 1vw',
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        <img
+          src={podcast['im:image'][2].label}
+          alt={podcast['im:name'].label}
+          style={{
+            boxSizing: 'border-box',
+            border: '1px solid #f2f2f2',
+            borderRadius: '50%',
+            position: 'relative',
+            margin: '0vw 3vw',
+            top: '-7vw',
+            width: 'calc(100% - 6vw)',
+          }}
+        />
+        <h5 style={{ marginTop: '-7vw', textTransform: 'uppercase' }}>{podcast['im:name'].label}</h5>
+        <p style={{ fontSize: '0.7em'}}>Author: {podcast['im:artist'].label}</p>
+      </div>
+    </div>
   );
 }
 
