f8fb9db2da84c7e95472b087dd068eff1c849b96
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-18 16:41:41 +0200

    refactor: podcasts list styles into a separate file

--- src/components/PodcastsList/index.tsx
+++ src/components/PodcastsList/index.tsx
@@ -12,32 +12,18 @@ const PodcastList: React.FC<PodcastListPropsType> = ({
   podcasts,
   filter = ''
 }) => {
-
   const filteredPodcasts = podcasts?.filter((podcast) => 
     podcast['im:name'].label.toLowerCase().includes(filter.toLowerCase()) ||
     podcast['im:artist'].label.toLowerCase().includes(filter.toLowerCase())
   ) || [];
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        marginTop: '8%',
-        columnGap: '1vw',
-        rowGap: '6vw',
-        width: '100%',
-      }}
-    >
+    <div id='podcasts-list-container'>
       {filteredPodcasts.map((podcast) =>
         <PodcastCard key={podcast.id.attributes['im:id']} podcast={podcast} />
       )}
     </div>
   );
-}
-
-
+};
 
 export default PodcastList;
