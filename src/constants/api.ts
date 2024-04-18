83dee1744499fca009b89bef73baadf2dc17be5d
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-18 11:58:13 +0200

    chore: raising react-query cache time to 1 day

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,4 +2,4 @@ export const PODCASTS_URL = 'https://itunes.apple.com/us/rss/toppodcasts/limit=1
 export const PODCAST_DETAIL_URL = (id: string, limit = 20): string =>
     `https://itunes.apple.com/lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=${limit}`;
 
-export const CACHE_TIME = 1000 * 60 * 10; // 10 minutes
+export const CACHE_TIME = 1000 * 60 * 60 * 24; // 1 day
