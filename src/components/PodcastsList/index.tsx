1f6078b47eaedd1b3096aad72f7397a86be56465
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-19 21:06:02 +0200

    chore: make podcasts list more modular and allow passing the podcast item as prop

--- src/components/PodcastsList/index.tsx
+++ src/components/PodcastsList/index.tsx
@@ -1,34 +1,38 @@
 import { useNavigate } from "react-router-dom";
-
 import PodcastCard from '../PodcastCard';
 import { Podcast } from "../../constants/types";
-
 import './index.css';
+
+const useFilteredPodcasts = (podcasts: Podcast[], filter: string) =>
+  podcasts.filter((podcast) => 
+    podcast['im:name'].label.toLowerCase().includes(filter.toLowerCase()) ||
+    podcast['im:artist'].label.toLowerCase().includes(filter.toLowerCase())
+  );
 
 type PodcastListPropsType = {
   podcasts: Podcast[];
   filter: string;
+  PodcastComponent?:
+    React.FC<{podcast: Podcast, onPodcastClick: (id: string) => void}>;
 };
 
 const PodcastList: React.FC<PodcastListPropsType> = ({
   podcasts,
-  filter = ''
+  filter = '',
+  PodcastComponent = PodcastCard
 }) => {
   const navigate = useNavigate();
 
-  const onPodcastClickHandler = (id: string) => {
+  const onPodcastClickHandler = (id?: string) => {
     navigate(`/podcast/${id}`);
   };
 
-  const filteredPodcasts = podcasts?.filter((podcast) => 
-    podcast['im:name'].label.toLowerCase().includes(filter.toLowerCase()) ||
-    podcast['im:artist'].label.toLowerCase().includes(filter.toLowerCase())
-  ) || [];
+  const filteredPodcasts = useFilteredPodcasts(podcasts, filter);
 
   return (
     <div id='podcasts-list-container'>
       {filteredPodcasts.map((podcast) =>
-        <PodcastCard
+        <PodcastComponent
           key={podcast.id.attributes['im:id']}
           podcast={podcast}
           onPodcastClick={onPodcastClickHandler}
