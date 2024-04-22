e6c741a8883da177d0eb7be018aea411efd8cd8e
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-22 23:00:17 +0200

    refactor: optimize hooks removing unnecesary state

--- src/hooks/usePodcastDetailById.ts
+++ src/hooks/usePodcastDetailById.ts
@@ -1,27 +1,12 @@
-import { useEffect, useState } from 'react';
 import { useFetchPodcastDetailById } from '../services/usePodcastDataService';
 import { Episode, PodcastDetail } from '../constants/types';
 
 const usePodcastDetailById = (podcastId: string) => {
-  const [podcastDetail, setPodcastDetail] = useState<PodcastDetail | null>(null);
-  const [episodes, setEpisodes] = useState<Episode[]>([]);
-
   const { isLoading, data } = useFetchPodcastDetailById(podcastId);
 
-  useEffect(() => {
-    if (!isLoading && data) {
-      const detailData = data.results.find((data) => data.kind === 'podcast');
-      const episodeData = data.results.filter((data) => data.kind === 'podcast-episode');
-
-      if (detailData) {
-        setPodcastDetail(detailData as PodcastDetail);
-      }
-
-      if (episodeData?.length) {
-        setEpisodes(episodeData as Episode[]);
-      }
-    }
-  }, [isLoading, podcastId]);
+  const podcastDetail = data?.results.find((data) => data.kind === 'podcast') as PodcastDetail;
+  const episodes =
+    (data?.results.filter((data) => data.kind === 'podcast-episode') as Episode[]) || [];
 
   return {
     isLoading,
--- src/hooks/usePodcasts.ts
+++ src/hooks/usePodcasts.ts
@@ -1,21 +1,11 @@
-import { useEffect, useState } from 'react';
 import { useFetchPodcasts } from '../services/usePodcastDataService';
-import { Podcast } from '../constants/types';
 
 const usePodcastsHook = () => {
-  const [podcasts, setPodcasts] = useState<Podcast[]>([]);
-
   const { isLoading, data } = useFetchPodcasts();
-
-  useEffect(() => {
-    if (!isLoading) {
-      setPodcasts(data?.feed.entry || []);
-    }
-  }, [isLoading, data?.feed.entry]);
 
   return {
     isLoading,
-    podcasts,
+    podcasts: data?.feed.entry || [],
   };
 };
 
