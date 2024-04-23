2b376e99313b3d3b117089e0e0daeaecf91eb0e9
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-23 20:38:31 +0200

    chore: remove HomeScreen create-react-app test

--- src/screens/HomeScreen/HomeScreen.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import HomeScreen from '.';
-
-test('renders learn react link', () => {
-  render(<HomeScreen />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
