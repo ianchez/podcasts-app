4dde43db196b5084e732674bc7594c3862825097
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-23 18:05:54 +0200

    refactor: undo changes about passing the urls of the fetch into the react-query hooks

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,4 +1,5 @@
-import { CORS_SERVICE_URL } from '../constants/api';
+import { PodcastDetailResultData, PodcastsResultData } from 'src/constants/types';
+import { CORS_SERVICE_URL, PODCASTS_URL, PODCAST_DETAIL_URL } from '../constants/api';
 import HttpClient from './HttpClient';
 
 class ApiService {
@@ -7,9 +8,13 @@ class ApiService {
     private corsServiceUrl: string,
   ) {}
 
-  async getData(url: string): Promise<any> {
+  private async getData(url: string): Promise<any> {
     'use server';
     try {
+      if (!url) {
+        throw new Error('URL is required for fetching data.');
+      }
+
       const response = await this.httpClient.get(
         `${this.corsServiceUrl}?url=${encodeURIComponent(url)}`,
       );
@@ -23,6 +28,14 @@ class ApiService {
       console.error(error);
       throw error;
     }
+  }
+
+  public async getPodcasts(): Promise<PodcastsResultData> {
+    return this.getData(PODCASTS_URL);
+  }
+
+  public async getPodcastDetailById(id: string, limit?: number): Promise<PodcastDetailResultData> {
+    return this.getData(PODCAST_DETAIL_URL(id, limit));
   }
 }
 
--- src/services/usePodcastDataService.ts
+++ src/services/usePodcastDataService.ts
@@ -1,18 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 
 import ApiService from './ApiService';
-import { PODCASTS_URL, PODCAST_DETAIL_URL } from '../constants/api';
 import { PodcastsResultData, PodcastDetailResultData } from '../constants/types';
 
 export const useFetchPodcasts = () =>
   useQuery<PodcastsResultData>({
     queryKey: ['podcasts'],
-    queryFn: () => ApiService.getData(PODCASTS_URL),
+    queryFn: async () => await ApiService.getPodcasts(),
   });
 
 export const useFetchPodcastDetailById = (id: string, limit?: number) =>
   useQuery<PodcastDetailResultData>({
     queryKey: ['podcast', id, limit],
-    queryFn: () => ApiService.getData(PODCAST_DETAIL_URL(id, limit)),
+    queryFn: async () => await ApiService.getPodcastDetailById(id, limit),
     enabled: !!id,
   });
