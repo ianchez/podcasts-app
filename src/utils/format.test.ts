c7ddd3ea7806d88ca8fcb2e3a1925667c0851fd8
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-23 20:39:17 +0200

    chore: util format test

---
+++ src/utils/format.test.ts
@@ -0,0 +1,23 @@
+import { formatDuration } from './format';
+
+describe('Utils: formatDuration', () => {
+  it('should return 00:00 if input is 0', () => {
+    expect(formatDuration(0)).toBe('00:00');
+  });
+
+  it('should return 00:00 if input is undefined', () => {
+    expect(formatDuration()).toBe('00:00');
+  });
+
+  it('should return 00:01 if input is 1000', () => {
+    expect(formatDuration(1000)).toBe('00:01');
+  });
+
+  it('should return 01:01 if input is 61000', () => {
+    expect(formatDuration(61000)).toBe('01:01');
+  });
+
+  it('should return 01:01:01 if input is 3661000', () => {
+    expect(formatDuration(3661000)).toBe('1:01:01');
+  });
+});
